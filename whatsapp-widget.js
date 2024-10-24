b300c89e656495e0392cc135d5ef3d0b643e9fbe
jonathan <59666853+jlopezvaldez@users.noreply.github.com>
2024-10-24 11:50:14 -0700

    Update whatsapp-widget.js

--- whatsapp-widget.js
+++ whatsapp-widget.js
@@ -148,7 +148,7 @@
                 <a href="https://wa.me/${config.phoneNumber}" target="_blank" class="wa-chat-link">
                     <div class="wa-chat-content">
                         <div class="wa-chat-avatar">
-                            <img src="girl.jpg" alt="Support Agent"/>
+                            <img src="https://images.unsplash.com/photo-1699899657680-421c2c2d5064?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Support Agent"/>
                         </div>
                         <div class="wa-chat-message">
                             <div class="wa-chat-main-text">${config.mainMessage}</div>
